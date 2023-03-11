4ae62b3350d24456cf949be1169ba9093d9a5d28
DylanLaptop <dylannicolet@hotmail.com>
2023-03-11 18:13:34 +0400

    Fixed: All boiler plates

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "App.css";
+
+export default function App(){
+  return(
+    <section className="app">
+      <main>
+        <section className="input-container"></section>
+        <section className="input-container"></section>
+        <section className="input-container"></section>
+        <section className="input-container"></section>
+        <section className="input-container"></section>
+        <section className="input-container"></section>
+        <section className="input-container"></section>
+        <section className="input-container"></section>
+        <section className="input-container"></section>
+      </main>
+    </section>
+  )
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-
-    entry: './src/index.js',
-
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js'
-    },
-
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        })
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve("babel-loader"),
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ["file-loader"],
+      },
     ],
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
-        ]
-    }
-}
+  },
+};
