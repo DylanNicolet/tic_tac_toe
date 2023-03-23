9d8274bc1e7f62da20c0debbf3795a3125730f5c
Dylan Nicolet <dn@wshop.com>
2023-03-23 11:20:22 +0400

    Completed: Logic & CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "App.css";
-import WinModal from "./WinModal";
 
 export default function App(){
   //State management
-    let [player1, setPlayer1] = React.useState(true)
+    let [ xScore, setXScore ] = React.useState( localStorage.getItem( "xScore" ) ? Number( localStorage.getItem( "xScore" ) ) : 0 )
+    let [ oScore, setOScore ] = React.useState( localStorage.getItem( "oScore" ) ? Number( localStorage.getItem( "oScore" ) ) : 0 )
+    let [ tiesScore, setTiesScore ] = React.useState( localStorage.getItem( "tiesScore" ) ? Number( localStorage.getItem( "tiesScore" ) ) : 0 )
+
+    let [ player1, setPlayer1 ] = React.useState( true )
 
     let [gameState, setGameState] = React.useState({
       row1:"",
@@ -17,7 +20,7 @@ export default function App(){
       diagonalToLeft:""
     })
 
-    let [gameEnded, setGameEnded] = React.useState([false, ""])
+    let [ gameEnded, setGameEnded ] = React.useState( [ false, "" ] )
   
   //function to handle player action and game states
     function handlePlayerChoice(e) {
@@ -50,21 +53,65 @@ export default function App(){
 
       setPlayer1(previous => !previous)
     }
+  
+  //Function to force reload the page and thus reset the game  
+    function handleResetGame() {
+      window.location.reload(false)
+    }
+  
+  //Function to reset scores on local storage
+    function handleResetScore() {
+      localStorage.setItem( "xScore", 0 )
+      localStorage.setItem( "tiesScore", 0 )
+      localStorage.setItem( "oScore", 0 )
+      setXScore( 0 )
+      setTiesScore( 0 )
+      setOScore( 0 )
+      setGameEnded( false, "" )
+      window.location.reload(false)
+    }
 
   //useEffect hook to check for a winner everytime gameState changes
-  React.useEffect(() => {
-    setTimeout(() => {
-      for (const key of Object.keys(gameState)) {
-            if(gameState[key] === "xxx"){
-              setGameEnded([true, "x"])
-            }
-            else if(gameState[key] === "ooo"){
-              setGameEnded([true, "o"])
-            }
+    React.useEffect(() => {
+      setTimeout(() => {
+        for (const key of Object.keys(gameState)) {
+          if(gameState[key] === "xxx"){
+            setXScore(xScore+1)
+            localStorage.setItem( "xScore", xScore+1 )
+            setGameEnded( [ true, "x" ] )
           }
-    }, 1300)
+          else if(gameState[key] === "ooo"){
+            setOScore(oScore+1)
+            localStorage.setItem( "oScore", oScore+1 )
+            setGameEnded( [ true, "o" ] )
+          }
+        }
+
+        if ( Object.values( gameState ).every( item => item.length === 3 ) && Object.values( gameState ).every( item => (item != "xxx" || item != "ooo") ) ) {
+          setTiesScore(tiesScore+1)
+          localStorage.setItem( "tiesScore", tiesScore+1 )
+          setGameEnded( [ true, "tie" ] )
+        }
+      }, 1000)
+      
+    }, [ gameState ] )
+  
+  //useEffect hook to check if scores already exist on local, if not then create them
+    React.useEffect( () => {
+      !localStorage.getItem( "xScore" ) && localStorage.setItem( "xScore", 0 )
+      !localStorage.getItem( "oScore" ) && localStorage.setItem( "oScore", 0 )
+      !localStorage.getItem("tiesScore") && localStorage.setItem("tiesScore", 0)
+    },[])
     
-  }, [gameState])
+  let winnerColor = ""
+  if ( gameEnded[ 1 ] === "x" ) {
+    winnerColor = "winner--blue"
+  }
+  else if ( gameEnded[ 1 ] === "o" ) {
+    winnerColor = "winner--yellow"
+  } else {
+    winnerColor = "winner--grey"
+  }
 
   return(
     <section className="app">
@@ -76,13 +123,13 @@ export default function App(){
           </section>
 
           {player1 ? 
-            <section className="header__player-turn"><span class="player">x</span><span class="turn">TURN</span></section>
+            <section className="header__player-turn"><span className="player">x</span><span className="turn">TURN</span></section>
              : 
-            <section className="header__player-turn"><span class="player">o</span><span class="turn">TURN</span></section>
+            <section className="header__player-turn"><span className="player">o</span><span className="turn">TURN</span></section>
           }
 
           <section className="header__reset-button__container">
-            <button type="button" className="header__reset-button">{'\u21BB'}</button>
+            <button type="button" className="header__reset-button" onClick={handleResetGame}>{'\u21BB'}</button>
           </section>
           
         </section>
@@ -98,8 +145,40 @@ export default function App(){
           <section className="input-container row3 column2" onClick={e => handlePlayerChoice(e)}></section>
           <section className="input-container row3 column3 diagonalToRight" onClick={e => handlePlayerChoice(e)}></section>
         </section>
+
+        <section className="footer">
+          <section className="footer_x-score">
+            <p>X (PLAYER 1)</p>
+            <strong>{xScore}</strong>
+          </section>
+          <section className="footer_ties-score">
+            <p>TIES</p>
+            <strong>{tiesScore}</strong>
+          </section>
+          <section className="footer_o-score">
+            <p>O (PLAYER 2)</p>
+            <strong>{oScore}</strong>
+          </section>
+        </section>
+
+        
       </main>
-      {gameEnded[0] && <WinModal winner={gameEnded[1]} />}
+
+      {gameEnded[ 0 ] &&
+       (<section className="win-modal">
+          <section className="win-modal__content">
+          {gameEnded[1] != "tie" && <p>YOU WON!</p>}
+          <span class={winnerColor} id={gameEnded[ 1 ] === "tie" ? "--tie" : ""}>
+            <strong id="strong">{gameEnded[ 1 ] === "tie" ? "" : gameEnded[ 1 ]}</strong>
+            {gameEnded[ 1 ] === "tie" ? "IT'S A TIE" : " TAKES THE ROUND"}
+          </span>
+          <section>
+            <button type="button" className="reset-score__button" onClick={handleResetScore}>QUIT</button>
+            <button type="button" className="reset-game__button" onClick={handleResetGame}>NEXT ROUND</button>
+          </section>
+          </section>
+        </section>)
+      }
     </section>
   )
 }
--- src/WinModal.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function WinModal(props){
-    return(
-        <section className="win-modal">
-            {props.winner} TAKES THE ROUND
-        </section>
-    )
-}
